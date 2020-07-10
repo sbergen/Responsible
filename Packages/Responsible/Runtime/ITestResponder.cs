b79274b6f219dafd849d5fd2d72e2e5dc487d829
Sakari Bergen <sakari@yousician.com>
2020-07-10 23:40:25 +0300

    Update documentation on ITestResponder

--- Packages/Responsible/Runtime/ITestResponder.cs
+++ Packages/Responsible/Runtime/ITestResponder.cs
@@ -4,11 +4,11 @@ namespace Responsible
 	/// Represents a test instruction which can be conditionally executed, if some condition is met.
 	/// Usually constructed from a <see cref="ITestWaitCondition{T}"/> and <see cref="ITestInstruction{T}"/>
 	/// </summary>
-	/// <remarks>
-	/// This is really just an alias to make code look cleaner and add semantics.
-	/// </remarks>
 	public interface ITestResponder<out T> : ITestOperationContext
 	{
+		// Design note: This interface is not just an alias for ITestWaitCondition<ITestInstruction<T>>
+		// in order to make it less easy to end up accidentally waiting for an ITestInstruction<T>.
+		// This property should almost always be used only internally.
 		ITestWaitCondition<ITestInstruction<T>> InstructionWaitCondition { get; }
 	}
 }
