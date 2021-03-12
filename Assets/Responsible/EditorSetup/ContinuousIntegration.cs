a182a4af9bafb9ecbb098d72b7cb3dca3c0b4a9d
Sakari Bergen <s@beatwaves.net>
2021-03-12 17:21:25 +0200

    Add ReSharper action (#33)
    Add ReSharper inspectcode action and reorganize DotSettings a bit.

--- Assets/Responsible/EditorSetup/ContinuousIntegration.cs
+++ Assets/Responsible/EditorSetup/ContinuousIntegration.cs
@@ -16,6 +16,11 @@ namespace Responsible.EditorSetup
         private static readonly string RepositoryPath = new DirectoryInfo(Application.dataPath).Parent.FullName;
         private static readonly string DocFxDir = Path.Combine(RepositoryPath, "docfx");
         private static readonly string DocFxJsonPath = Path.Combine(DocFxDir, "docfx.json");
+        private static readonly string ResharperDir = Path.Combine(RepositoryPath, "resharper");
+        private static readonly string ResharperResults = Path.Combine(ResharperDir, "inspect.xml");
+        private static readonly string ResharperStdout = Path.Combine(ResharperDir, "stdout.txt");
+        private static readonly string ResharperStderr = Path.Combine(ResharperDir, "stderr.txt");
+        private static readonly string DotSettingsFile = Path.Combine(RepositoryPath, "Responsible.sln.DotSettings");
 
         /// <summary>
         /// Build documentation from within Unity,
@@ -43,6 +48,40 @@ namespace Responsible.EditorSetup
             else
             {
                 Debug.Log($"Finished building documentation:\n{stdout}");
+            }
+        }
+
+        /// <summary>
+        /// Run the JetBrains inspectcode tool from within Unity,
+        /// to make this easier to use with existing Unity-related CI infrastructure.
+        /// ReSharper requires Unity dlls, so this can't be run without Unity around.
+        /// </summary>
+        [MenuItem("CI/Inspect Code")]
+        public static void InspectCode()
+        {
+            Debug.Log("Syncing solution...");
+            var solution = SyncSolution();
+
+            Debug.Log("Running inspections...");
+            var (status, stdout, stderr) = RunCommand(
+                workingDir: RepositoryPath,
+                command: "inspectcode",
+                Quote(solution),
+                $"-o={Quote(ResharperResults)}",
+                "-s=WARNING",
+                $"--profile={Quote(DotSettingsFile)}");
+
+            File.WriteAllText(ResharperStdout, stdout);
+            File.WriteAllText(ResharperStderr, stderr);
+
+            if (status != 0)
+            {
+                Debug.LogError($"Inspecting code failed:\n{stderr}");
+                throw new Exception($"Inspecting code failed ({status})");
+            }
+            else
+            {
+                Debug.Log($"Finished inspecting code:\n{stdout}");
             }
         }
 
