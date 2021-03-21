a810fdf3521c420ce037fc75660a728a7f5debb5
Sakari Bergen <sakari@yousician.com>
2021-03-21 18:47:31 +0200

    Add MonadTests

---
+++ Assets/Responsible/Tests/Runtime/NoRx/MonadTests.cs
@@ -0,0 +1,47 @@
+using System;
+using NUnit.Framework;
+using Responsible.NoRx;
+using static Responsible.NoRx.Responsibly;
+
+namespace Responsible.Tests.Runtime.NoRx
+{
+	public class MonadTests : ResponsibleTestBase
+	{
+		// These tests don't really prove anything, but are a nice exercise :)
+		// They miss the "for all" aspect...
+
+		[Test]
+		public void LeftIdentity()
+		{
+			ITestInstruction<int> MultiplyByTwo(int value) => Return(2 * value);
+			this.AssertEqual(
+				Return(2).ContinueWith(MultiplyByTwo),
+				MultiplyByTwo(2));
+		}
+
+		[Test]
+		public void RightIdentity()
+		{
+			// As we capture caller context in Return, can't just use Return<int> here :/
+			this.AssertEqual(
+				Return(2).ContinueWith(val => Return(val)),
+				Return(2));
+		}
+
+		[Test]
+		public void Associativity()
+		{
+			ITestInstruction<int> MultiplyByTwo(int value) => Return(2 * value);
+			ITestInstruction<int> AddOne(int value) => Return(value + 1);
+
+			this.AssertEqual(
+				Return(2).ContinueWith(MultiplyByTwo).ContinueWith(AddOne),
+				Return(2).ContinueWith(x => MultiplyByTwo(x).ContinueWith(AddOne)));
+		}
+
+		private void AssertEqual<T>(ITestInstruction<T> left, ITestInstruction<T> right)
+			=> Assert.AreEqual(
+				left.ToTask(this.Executor).Result,
+				right.ToTask(this.Executor).Result);
+	}
+}
