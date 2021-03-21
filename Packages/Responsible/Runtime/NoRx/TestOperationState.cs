9fa471dc6d5795bd6e9fea852b09620b07188fd7
Sakari Bergen <sakari@yousician.com>
2021-03-21 19:15:50 +0200

    Add state -> Task + StateLoggingTests

--- Packages/Responsible/Runtime/NoRx/TestOperationState.cs
+++ Packages/Responsible/Runtime/NoRx/TestOperationState.cs
@@ -1,3 +1,7 @@
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Responsible.NoRx.Context;
 using Responsible.NoRx.State;
 
 namespace Responsible.NoRx
@@ -8,6 +12,18 @@ namespace Responsible.NoRx
 	/// </summary>
 	public static class TestOperationState
 	{
+		public static Task<T> ToTask<T>(
+			this ITestOperationState<T> state,
+			TestInstructionExecutor executor,
+			CancellationToken cancellationToken = default,
+			[CallerMemberName] string memberName = "",
+			[CallerFilePath] string sourceFilePath = "",
+			[CallerLineNumber] int sourceLineNumber = 0)
+			=> executor.RunInstruction(
+				state,
+				new SourceContext(nameof(ToTask), memberName, sourceFilePath, sourceLineNumber),
+				cancellationToken);
+
 		internal static ITestOperationState<Nothing> AsNothingOperationState<T>(this ITestOperationState<T> state)
 			=> new NothingOperationState<T, Nothing>(state, _ => Nothing.Default);
 	}
