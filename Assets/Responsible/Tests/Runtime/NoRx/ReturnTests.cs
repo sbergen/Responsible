ffadb6cea67aa799d73f4b898f897757b613d2bb
Sakari Bergen <sakari@yousician.com>
2021-03-21 18:51:20 +0200

    Add ReturnTests

---
+++ Assets/Responsible/Tests/Runtime/NoRx/ReturnTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using Responsible.NoRx;
+using static Responsible.NoRx.Responsibly;
+
+namespace Responsible.Tests.Runtime.NoRx
+{
+	public class ReturnTests : ResponsibleTestBase
+	{
+		[Test]
+		public void Return_ReturnsCorrectValue()
+		{
+			var value = 42;
+			var returnInstruction = Return(value);
+			Assert.AreEqual(
+				42,
+				returnInstruction
+					.ToTask(this.Executor)
+					.Result);
+		}
+	}
+}
