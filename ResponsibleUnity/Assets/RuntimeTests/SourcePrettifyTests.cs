dee89fc7a8e47cdb970cc2adb1cfe727ffcf14ef
Sakari Bergen <sakari@yousician.com>
2021-04-18 19:57:37 +0300

    Handle source paths not in not in project

---
+++ ResponsibleUnity/Assets/RuntimeTests/SourcePrettifyTests.cs
@@ -0,0 +1,37 @@
+using System;
+using NUnit.Framework;
+using Responsible.Tests;
+using Responsible.Tests.Utilities;
+using static Responsible.Responsibly;
+
+namespace Responsible.UnityTests
+{
+	public class SourcePrettifyTests : ResponsibleTestBase
+	{
+		[Test]
+		public void SourcePath_IsPrettified_WhenInProject()
+		{
+			var task = Do("Throw", () => throw new Exception())
+				.ToTask(this.Executor);
+			var exception = GetFailureException(task);
+			StateAssert.StringContainsInOrder(exception.Message)
+				.Failed("Throw")
+				.Details(@"\(at Assets/RuntimeTests/SourcePrettifyTests\.cs\:");
+		}
+
+		[Test]
+		public void SourcePath_IsNotPrettified_WhenNotInProject()
+		{
+			var task = Do(
+					"Throw",
+					() => throw new Exception(),
+					// ReSharper disable once ExplicitCallerInfoArgument
+					sourceFilePath: "/foo/bar.cs")
+				.ToTask(this.Executor);
+			var exception = GetFailureException(task);
+			StateAssert.StringContainsInOrder(exception.Message)
+				.Failed("Throw")
+				.Details(@"\(at /foo/bar\.cs\:");
+		}
+	}
+}
