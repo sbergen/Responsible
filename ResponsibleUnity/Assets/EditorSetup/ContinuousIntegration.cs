b1819df4ff130f96b728c8b3dfe8cb867164ce66
Sakari Bergen <s@beatwaves.net>
2021-04-25 19:23:27 +0300

    Cleanup editor code (#88)
    * Remove ReSharper invoking Unity code (no longer used)
    * Move NSubstitute binaries to the UnityTests directory: This gets rid of the Assembly-CSharp assembly, which is otherwise unnecessary.

--- ResponsibleUnity/Assets/EditorSetup/ContinuousIntegration.cs
+++ ResponsibleUnity/Assets/EditorSetup/ContinuousIntegration.cs
@@ -2,9 +2,7 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Xml.Linq;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
@@ -18,10 +16,6 @@ namespace Responsible.EditorSetup
         private static readonly string RepositoryPath = new DirectoryInfo(Application.dataPath).Parent.Parent.FullName;
         private static readonly string DocFxDir = Path.Combine(RepositoryPath, "docfx");
         private static readonly string DocFxJsonPath = Path.Combine(DocFxDir, "docfx.json");
-        private static readonly string ResharperDir = Path.Combine(RepositoryPath, "resharper");
-        private static readonly string ResharperResults = Path.Combine(ResharperDir, "inspect.xml");
-        private static readonly string ResharperStdout = Path.Combine(ResharperDir, "stdout.txt");
-        private static readonly string ResharperStderr = Path.Combine(ResharperDir, "stderr.txt");
 
         /// <summary>
         /// Build documentation from within Unity,
@@ -36,7 +30,7 @@ namespace Responsible.EditorSetup
             SyncSolution();
 
             Debug.Log("Building documentation...");
-            var (status, stdout, _) = RunCommand(
+            var (status, stdout) = RunCommand(
                 workingDir: DocFxDir,
                 command: "docfx",
                 Quote(DocFxJsonPath), "--warningsAsErrors", "--force");
@@ -49,47 +43,6 @@ namespace Responsible.EditorSetup
             else
             {
                 Debug.Log($"Finished building documentation:\n{stdout}");
-            }
-        }
-
-        /// <summary>
-        /// Run the JetBrains inspectcode tool from within Unity,
-        /// to make this easier to use with existing Unity-related CI infrastructure.
-        /// ReSharper requires Unity dlls, so this can't be run without Unity around.
-        /// </summary>
-        [MenuItem("CI/Inspect Code")]
-        public static void InspectCode()
-        {
-            Debug.Log("Syncing solution...");
-            var solution = SyncSolution();
-
-            Debug.Log("Running inspections...");
-            var (status, stdout, stderr) = RunCommand(
-                workingDir: RepositoryPath,
-                command: "inspectcode",
-                Quote(solution),
-                $"-o={Quote(ResharperResults)}",
-                "-a", // Use absolute paths to make this work better in GitHub
-                "-s=WARNING");
-
-            File.WriteAllText(ResharperStdout, stdout);
-            File.WriteAllText(ResharperStderr, stderr);
-
-            if (status != 0)
-            {
-                Debug.LogError($"Inspecting code failed:\n{stderr}");
-                throw new Exception($"Inspecting code failed ({status})");
-            }
-            else
-            {
-                Debug.Log($"Finished inspecting code:\n{stdout}");
-
-                var document = XElement.Load(File.OpenRead(ResharperResults));
-                var issueCount = document.Descendants("Issue").Count();
-                if (issueCount > 0)
-                {
-                    throw new Exception($"Found {issueCount} code inspection issues.");
-                }
             }
         }
 
@@ -111,7 +64,7 @@ namespace Responsible.EditorSetup
 
         private static string Quote(string str) => $"\"{str}\"";
 
-        private static (int status, string stdout, string stderr) RunCommand(
+        private static (int status, string stdout) RunCommand(
             string workingDir,
             string command,
             params string[] args)
@@ -130,9 +83,8 @@ namespace Responsible.EditorSetup
             var process = Process.Start(processInfo);
             Assert.IsNotNull(process);
             var stdout = process.StandardOutput.ReadToEndAsync();
-            var stderr = process.StandardError.ReadToEndAsync();
             process.WaitForExit();
-            return (process.ExitCode, stdout.Result, stderr.Result);
+            return (process.ExitCode, stdout.Result);
         }
     }
 }
