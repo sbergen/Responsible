a903555f3f7f33eb089f2300a9a55bc557de8d08
Sakari Bergen <s@beatwaves.net>
2021-09-05 18:35:50 +0300

    Work around issue with long Unity Labels (#111)
    Fixes #110

---
+++ ResponsibleUnity/Assets/EditorTests/StringSplitterTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Responsible.Editor.Utilities;
+
+namespace Responsible.EditorTests
+{
+	public class StringSplitterTests
+	{
+		[TestCase(1, "", ExpectedResult = new object[] { "" })]
+		[TestCase(1, "\n", ExpectedResult = new object[] { "", "" })]
+		[TestCase(1, "foo", ExpectedResult = new object[] { "foo" })]
+		[TestCase(1, "foo\n", ExpectedResult = new object[] { "foo", "" })]
+		[TestCase(1, "foo\n\n", ExpectedResult = new object[] { "foo", "", "" })]
+		[TestCase(1, "foo\nbar", ExpectedResult = new object[] { "foo", "bar" })]
+		[TestCase(1, "foo\nbar\n", ExpectedResult = new object[] { "foo", "bar", "" })]
+		[TestCase(2, "", ExpectedResult = new object[] { "" })]
+		[TestCase(2, "foo", ExpectedResult = new object[] { "foo" })]
+		[TestCase(2, "\n", ExpectedResult = new object[] { "\n" })]
+		[TestCase(2, "\n\n", ExpectedResult = new object[] { "\n", "" })]
+		[TestCase(2, "foo\n", ExpectedResult = new object[] { "foo\n" })]
+		[TestCase(2, "foo\n\n", ExpectedResult = new object[] { "foo\n", "" })]
+		[TestCase(2, "foo\n\n\n", ExpectedResult = new object[] {  "foo\n", "\n" })]
+		[TestCase(2, "foo\n\n\n\n", ExpectedResult = new object[] {  "foo\n", "\n", "" })]
+		public string[] SubstringsOfMaxLines(int maxLines, string input) =>
+	        input.SubstringsOfMaxLines(maxLines).ToArray();
+
+		[Test]
+		public void SubstringsOfMaxLines_ReturnsInputAsReference_WhenNotSplit()
+		{
+			var input = "foo\n";
+			Assert.AreSame(input, input.SubstringsOfMaxLines(2).Single());
+		}
+
+		[TestCase(0)]
+		[TestCase(-1)]
+		public void SubstringsOfMaxLines_Throws_WhenMaxLinesLessThanOrEqualToZero(int maxLines)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(
+				// ReSharper disable once ReturnValueOfPureMethodIsNotUsed, should throw
+				() => string.Empty.SubstringsOfMaxLines(maxLines).ToArray());
+		}
+	}
+}
