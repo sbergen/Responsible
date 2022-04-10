80d8a3d93e9b58f1499067f118dad3a2871f6a36
Sakari Bergen <sakari@yousician.com>
2022-04-10 19:08:29 +0300

    Revert accidental unindent

--- com.beatwaves.responsible/Samples~/ResponsibleGame/PlayModeTests/BddStyleTests.cs
+++ com.beatwaves.responsible/Samples~/ResponsibleGame/PlayModeTests/BddStyleTests.cs
@@ -7,17 +7,17 @@ using static Responsible.Responsibly;
 
 namespace ResponsibleGame.PlayModeTests
 {
-// Example of BDD-style tests
-[Feature("Restarting the game")]
-public class BddStyleTests : SystemTest
-{
-	[Scenario("The game should be restartable easily after failure")]
-	public IBddStep[] Restart() => new[]
+	// Example of BDD-style tests
+	[Feature("Restarting the game")]
+	public class BddStyleTests : SystemTest
 	{
-		Given("the player has failed", this.FailTheGame()),
-		When("the user presses the trigger key", this.MockTriggerInput()),
-		Then("the game should be restarted", this.AssertTheGameHasBeenRestarted())
-	};
+		[Scenario("The game should be restartable easily after failure")]
+		public IBddStep[] Restart() => new[]
+		{
+			Given("the player has failed", this.FailTheGame()),
+			When("the user presses the trigger key", this.MockTriggerInput()),
+			Then("the game should be restarted", this.AssertTheGameHasBeenRestarted())
+		};
 
 		private ITestInstruction<object> FailTheGame()
 		{
