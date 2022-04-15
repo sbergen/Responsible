e0c8bb851c0bcae6bf3ab32d1e978d918db1543b
Sakari Bergen <sakari@yousician.com>
2022-04-15 13:39:38 +0300

    Exclude Main from coverage (it's not implemented yet)

--- ResponsibleGherkin/Program.cs
+++ ResponsibleGherkin/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ResponsibleGherkin;
+
+[ExcludeFromCodeCoverage]
+public static class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("I'm not ready to be invoked from the CLI Yet!");
+	}
+}
