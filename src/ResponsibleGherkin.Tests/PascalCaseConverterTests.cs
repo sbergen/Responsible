de3b8c11c8868562806e64f3eeeda71b016ba4b2
Sakari Bergen <sakari@yousician.com>
2022-04-17 15:27:51 +0300

    Implement my own ToPascalCase, the previous one didn't work well

--- src/ResponsibleGherkin.Tests/PascalCaseConverterTests.cs
+++ src/ResponsibleGherkin.Tests/PascalCaseConverterTests.cs
@@ -9,11 +9,13 @@ public class PascalCaseConverterTests
 	[Theory]
 	[InlineData("", "")]
 	[InlineData(" ", "")]
-	[InlineData(" foo ", "Foo")]
-	[InlineData(" lots\nof\tfood ", "LotsOfFood")]
-	[InlineData(" lots     of      food ", "LotsOfFood")]
+	[InlineData("  foo  ", "Foo")]
+	[InlineData("foo   a  bar", "FooABar")]
+	[InlineData("FooBar", "FooBar")]
+	[InlineData("Foo 1 bar", "Foo1Bar")]
+	[InlineData("1 bar", "_1Bar")]
 	[InlineData("DSP scheduler", "DspScheduler")]
-	[InlineData("DspScheduler can do", "DspSchedulerCanDo")]
+	[InlineData("!this-should-be-fine!", "ThisShouldBeFine")]
 	public void ConvertToPascalCase_ReturnsExceptedValue(string input, string expected) =>
 		PascalCaseConverter.ConvertToPascalCase(input).Should().Be(expected);
 }
