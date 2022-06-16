f5c3a04bb208be49334990ff1dc14fb8eb4a0229
Sakari Bergen <sakari@yousician.com>
2022-06-16 22:08:32 +0300

    Script tweaking

--- ResponsibleUnity/run-tests.sh
+++ ResponsibleUnity/run-tests.sh
@@ -4,8 +4,6 @@
 # assuming that repository is mounted at /project
 # and a license file is at /project/unity-licesnse
 
-set -e
-
-mkdir /project/TestResults
+mkdir -p /project/TestResults
 unity-editor -batchmode -logFile /dev/stdout -manualLicenseFile /project/unity-license
 unity-editor -batchmode -nographics -disable-assembly-updater -enableCodeCoverage -debugCodeOptimization -executeMethod Responsible.EditorSetup.TestRunner.RunTests -logFile /dev/stdout -projectPath /project/ResponsibleUnity -coverageResultsPath /project/TestResults -coverageOptions assemblyFilters:+Responsible,+Responsible.Editor
